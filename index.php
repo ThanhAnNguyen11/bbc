cb4ca59eb0fa4c0679c60566897a262b7ed2bff0
annt7@vng.com.vn <annt7@vng.com.vn>
2018-10-13 11:53:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include($_SERVER["DOCUMENT_ROOT"] . "/bbc/app_config.php");
+include(APP_PATH."libs/head.php"); 
+?>
+</head>
+
+<body id="top">
+<!--===================================================-->
+<div id="wrapper">
+<!--===================================================-->
+<!--Header-->
+<?php include(APP_PATH."libs/header.php"); ?>
+<!--/Header-->
+
+<p>Test</p>
+
+<!--Footer-->
+<?php include(APP_PATH."libs/footer.php"); ?>
+<!--/Footer-->
+<!--===================================================-->
+</div>
+<!--/wrapper-->
+<!--===================================================-->
+
+</body>
+</html>	
