4bdf27331de9d3a9bf8fbb8e64951e643e46839e
annt7@vng.com.vn <annt7@vng.com.vn>
2018-10-13 12:04:15 +0700

    delete admin folder

--- admin/wp-content/themes/template/footer.php
+++
@@ -1,8 +0,0 @@
-footer	
-<?
-wp_footer();
-?>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript" src="/common/js/common.js"></script>
-<script type="text/javascript" src="/common/js/smoothscroll.js"></script>
-</script>
